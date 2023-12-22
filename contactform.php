8563f7cfd4f8d758810975e437a0a9e77df5f6eb
Bavli Armanyous <bavli898@gmail.com>
2023-12-22 20:55:46 +0100

    tutorialfiles

---
+++ contactform.php
@@ -0,0 +1,16 @@
+<?php
+if (isset($_POST['submitButton'])) {
+    $name =$_POST['Voor- en Achternaam'];
+    $subject =$_POST['onderwerp'];
+    $mailFrom =$_POST['Emailadres'];
+    $mailTo = "Bavli2002@hotmail.com";
+    $message =$_POST['Bericht'];
+    $headers = "From: ".$mailFrom;
+    $txt ="Je hebt een email ontvangen van ".$name.".\n\n".$message;
+
+
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
+?>
