28595f27b051047f88f4caca51cf4d710e003e51
Bavli Armanyous <bavli898@gmail.com>
2024-02-04 23:13:30 +0100

    -

--- contactform.php
+++ contactform.php
@@ -3,7 +3,7 @@ if (isset($_POST['submitButton'])) {
     $name =$_POST['Voor- en Achternaam'];
     $subject =$_POST['onderwerp'];
     $mailFrom =$_POST['Emailadres'];
-    $mailTo = "Bavli2002@hotmail.com";
+    $mailTo = "Info@chalgado.com.com";
     $message =$_POST['Bericht'];
     $headers = "From: ".$mailFrom;
     $txt ="Je hebt een email ontvangen van ".$name.".\n\n".$message;
